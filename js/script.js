c7d4719a11cd2651d20bccde9dfa8aff8f3dc0e8
Wiltencir Garcia <wiltencir@gmail.com>
2020-04-27 19:24:00 -0300

    First release

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var url = "https://blog.canaisgratis.org/player3/canaishlb.php?canal=";
+
+$(function() {
+    changeChannel('disney');
+});
+        
+function changeChannel(channel){
+    document.getElementById("channel").innerHTML = "";
+
+    if(channel != null){
+        document.getElementById("channel").innerHTML = "<iframe name=\"Player\" id=\"Player\"src="+url+channel+" frameborder=\"0\" height=\"800\" scrolling=\"no\" width=\"100%\" allowfullscreen=\"\"> </iframe>";
+    }
+}
